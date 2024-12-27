b11d467b909a353094a09859c7cafe96fda6b195
Dmitrii Povolotskii <povolotskii.dmitrii@huawei.com>
2024-12-27 09:28:26 +0000

    WIP driver test

---
+++ wasm/driver_test.go
@@ -0,0 +1,21 @@
+package wasm
+
+import "testing"
+
+func TestSetConfig(t *testing.T) {
+	tCases := []struct {
+		name           string
+		config         []byte
+		expectedErrMsg string
+	}{
+		{
+			
+		},
+	}
+
+	for _, tCase := range tCases {
+		t.Run(tCase.name, func(t *testing.T) {
+
+		})
+	}
+}
